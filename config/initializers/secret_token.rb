ac734e3581a81326a6a8b4a1f047c323c584d590
Ilya Katz <ilya@rentini.com>
2012-04-28 12:51:28 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsreply::Application.config.secret_token = '18018e5a84a36be5587b005500e561d3030a9946705dda687be8ac9994f03b2ad0cd426fcab24e3ba9a0bfda13bbf0bf5e7075110c32d2202b659921e0766dde'
